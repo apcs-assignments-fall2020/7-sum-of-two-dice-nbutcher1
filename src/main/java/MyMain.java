40358f79609d3846ab5258dab408c572ec906130
nbutcher1 <22nbutcher@athenian.org>
2020-09-27 23:35:11 +0000

    finished

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.Arrays;
 public class MyMain {
 
     // Rolls a single die
     // Returns a random integer between 1 and 6
     public static int rollDie() {
         Random rand = new Random();
-        int dice = rand.nextInt(7);
+        int dice = rand.nextInt(6);
+        dice += 1;
         return dice;
     }
 
@@ -15,10 +17,11 @@ public class MyMain {
     // the number of times a certain sum of two dice was rolled
     public static int[] sumOfTwoDice(int n) {
         int[] ok = {0,0,0,0,0,0,0,0,0,0,0};
-        for (int i = 0; i < n; i++); {
+        for (int i = 0; i < n; i++) {
             int dice1 = rollDie();
             int dice2 = rollDie();
             int total = dice1 + dice2;
+            System.out.println(total);
             ok[total-2] += 1;
 
         }
@@ -28,6 +31,6 @@ public class MyMain {
 
 
     public static void main(String[] args) {
-        System.out.println(sumOfTwoDice(10));
+        System.out.println((sumOfTwoDice(10));
     }
 }
